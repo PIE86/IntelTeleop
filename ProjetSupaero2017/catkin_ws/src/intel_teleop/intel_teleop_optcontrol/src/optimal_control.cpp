700e6a5f946d0e32b6d275e1678c2b94767acf79
Diane Bury <diane.bury@gmail.com>
2016-12-12 20:08:29 +0100

    ROS skeleton

---
+++ ProjetSupaero2017/catkin_ws/src/intel_teleop/intel_teleop_optcontrol/src/optimal_control.cpp
@@ -0,0 +1,45 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+#include <intel_teleop_msgs/DroneState.h>
+#include <intel_teleop_msgs/SpeedControl.h>
+#include <intel_teleop_msgs/MotorControl.h>
+#include <intel_teleop_msgs/UserInput.h>
+
+void estimated_state_feedback(const intel_teleop_msgs::DroneState estimation)
+{
+  ROS_INFO("Optimal Control heard estimated position");
+}
+
+void node_loop()
+{
+    ROS_INFO("%s", "optimal_control loop");
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "optimal_control")
+  ros::NodeHandle n;
+
+  ros::Publisher current_position_topic = n.advertise<r2robotics_msgs::RobotPosition>("speed_control", 1000);
+  ros::Subscriber encoder_feedback_topic = n.subscribe("estimated_state", 1000, estimated_state_feedback); 
+
+  
+  ros::Rate loop_rate(1);
+
+  loop_rate.sleep();
+
+  int count = 0;
+  while (ros::ok())
+  {
+    node_loop()
+    
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+  return 0;
+}
