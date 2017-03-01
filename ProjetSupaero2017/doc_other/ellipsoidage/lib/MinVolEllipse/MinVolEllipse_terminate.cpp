2527aada5f43c4db42192552253f99a1c760cc24
Steve Ravalisse <steveravalisse@gmail.com>
2017-03-01 10:16:58 +0100

    matlab and c++ files for ellipsoid generation

---
+++ ProjetSupaero2017/doc_other/ellipsoidage/lib/MinVolEllipse/MinVolEllipse_terminate.cpp
@@ -0,0 +1,31 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: MinVolEllipse_terminate.cpp
+//
+// MATLAB Coder version            : 3.0
+// C/C++ source code generated on  : 27-Feb-2017 18:00:05
+//
+
+// Include Files
+#include "rt_nonfinite.h"
+#include "MinVolEllipse.h"
+#include "MinVolEllipse_terminate.h"
+
+// Function Definitions
+
+//
+// Arguments    : void
+// Return Type  : void
+//
+void MinVolEllipse_terminate()
+{
+  // (no terminate code required)
+}
+
+//
+// File trailer for MinVolEllipse_terminate.cpp
+//
+// [EOF]
+//
