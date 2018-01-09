76ab1fec5762f0f5d732ecf1839c3466db29f186
mfourmy <mederic.fourmy@gmail.com>
2018-01-09 13:14:40 +0100

    catkin_make done + README edit

--- ProjetSupaero2018/README.md
+++ ProjetSupaero2018/README.md
@@ -1,1 +1,8 @@
 TODO
+
+
+## Pense bête
+#### Ajouter le projet au ROS_PACKAGE_PATH: 
+Se placer dans catkin_ws du projet 2018 puis:
+`source devel/setup.bash`
+
---
+++ ProjetSupaero2018/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mfourmy/Documents/Sup/cours/3A/PIE/MemoryEnhancedPredictiveControl/ProjetSupaero2018/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/openrobots/lib/:/opt/openrobots/lib64/"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/openrobots/lib/pkgconfig"
+export PWD="/media/storage/Documents/Sup/cours/3A/PIE/MemoryEnhancedPredictiveControl/ProjetSupaero2018/catkin_ws/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:/opt/openrobots/lib/python2.7/dist-packages:/opt/openrobots/lib/python2.7/site-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mfourmy/Documents/Sup/cours/3A/PIE/MemoryEnhancedPredictiveControl/ProjetSupaero2018/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mfourmy/Documents/Sup/cours/3A/PIE/MemoryEnhancedPredictiveControl/ProjetSupaero2018/catkin_ws/src:$ROS_PACKAGE_PATH"
