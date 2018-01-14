710c135b54863b06b2743078bf3cd103b33d2af7
williamjussiau <william.jussiau@supaero.isae.fr>
2018-01-14 16:13:06 +0100

    Renamed package & functions: roadmap -> pie86_obstacles, for the sake of relevance check_point server & client fully working

---
+++ ProjetSupaero2018/catkin_ws/src/pie86_obstacles/scripts/check_point_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+from obstacles.srv import *
+
+global SERVICE_NAME
+SERVICE_NAME = 'check_point'
+
+def check_point_client(x,y):
+    rospy.wait_for_service(SERVICE_NAME)
+    try:
+        get_if_valid = rospy.ServiceProxy(SERVICE_NAME, CheckPoint)
+        resp = get_if_valid(x,y)
+        return resp.is_valid
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "Expecting two arguments: point coordinates x y"
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = float(sys.argv[1])
+        y = float(sys.argv[2])
+    else:
+        print usage()
+        sys.exit(1)
+    print "Requesting check on point: %s %s"%(x,y)
+    is_valid = check_point_client(x,y)
+    print "After checking point: point is valid? %s"%(is_valid)
