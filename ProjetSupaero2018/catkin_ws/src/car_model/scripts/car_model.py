8e910b7d4816413c3afed8e2b59c44ad69f6a8aa
martinRenou <martin.renou@gmail.com>
2018-01-15 14:01:43 +0100

    Create car_position topic

---
+++ ProjetSupaero2018/catkin_ws/src/car_model/scripts/car_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+
+
+class CarModel:
+    def __init__(self, init_x, init_y):
+        """ Create a car given its initial position
+        """
+        self.x = init_x
+        self.y = init_y
+
+    def set_position(self, dx, dy):
+        """ Change the position of the car given a control value (dx, dy)
+        """
+        self.x += dx
+        self.y += dy
+
+
+if __name__ == '__main__':
+    try:
+        # Create the car model
+        car_model = CarModel()
+
+        publisher = rospy.Publisher('car_position', String)
+        rospy.init_node('car_model', anonymous=True)
+
+        # Publish rate in Hz
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            rospy.loginfo(
+                'car_position: {}, {}'.format(car_model.x, car_model.y))
+            publisher.publish('{},{}'.format(car_model.x, car_model.y))
+            rate.sleep()
+
+    except rospy.ROSInterruptException:
+        pass
