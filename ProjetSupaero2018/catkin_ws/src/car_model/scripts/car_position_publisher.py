ef0b4b3e838302dad27fedda976e355814c95591
martinRenou <martin.renou@gmail.com>
2018-01-23 12:04:09 +0100

    Subscribe to the car_command topic

--- ProjetSupaero2018/catkin_ws/src/car_model/scripts/car_position_publisher.py
+++ ProjetSupaero2018/catkin_ws/src/car_model/scripts/car_position_publisher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Point
+from controller.msg import Command2D
 
 COMMAND_TOPIC = 't_car_command'
 CURRENT_POSITION_TOPIC = 't_car_position'
@@ -13,7 +14,7 @@ class CarModel:
         self.x = init_x
         self.y = init_y
 
-    def set_position(self, dx, dy):
+    def command(self, dx, dy):
         """ Change the position of the car given a control value (dx, dy)
         """
         self.x += dx
@@ -25,6 +26,14 @@ if __name__ == '__main__':
         # Create the car model
         car_model = CarModel(0.0, 0.0)
 
+        # Callback for the command
+        def callback(data):
+            car_model.command(data.dx, data.dy)
+
+        # Subscribe to the COMMAND_TOPIC
+        rospy.Subscriber(COMMAND_TOPIC, Command2D, callback)
+
+        # Create the CURRENT_POSITION_TOPIC
         publisher = rospy.Publisher(
             CURRENT_POSITION_TOPIC, Point, queue_size=10)
         rospy.init_node('car_model', anonymous=True)
@@ -32,9 +41,11 @@ if __name__ == '__main__':
         # Publish rate in Hz
         rate = rospy.Rate(10)
 
+        # Car position message
         car_position_msg = Point()
         car_position_msg.z = 0.0
 
+        # Publish the car position on the CURRENT_POSITION_TOPIC
         while not rospy.is_shutdown():
             car_position_msg.x = car_model.x
             car_position_msg.y = car_model.y
@@ -42,6 +53,7 @@ if __name__ == '__main__':
             rospy.loginfo('car_position: {}, {}, {}'.format(
                 car_position_msg.x, car_position_msg.y, car_position_msg.z))
             publisher.publish(car_position_msg)
+
             rate.sleep()
 
     except rospy.ROSInterruptException:
