8d0541bc9a5afe099bbd96e4babbeaa4d971d602
Zeman <janhonza.zeman@gmail.com>
2018-01-31 19:28:22 +0000

    coucou

---
+++ ProjetSupaero2018/catkin_ws/src/display/plugins/car_control_plugin.cc
@@ -0,0 +1,101 @@
+#ifndef _CAR_CONTROL_PLUGIN_HH_
+#define _CAR_CONTROL_PLUGIN_HH_
+
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/math/Vector3.hh>
+#include <gazebo/math/Pose.hh>
+#include <gazebo/math/Quaternion.hh>
+
+#include <stdio.h>
+
+#include <thread>
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "display/Control.h"
+
+#include <math.h>
+
+#define PI 3.14159265
+
+namespace gazebo
+
+{
+  class CarControlPlugin : public ModelPlugin
+  {
+
+  	public: CarControlPlugin() {}
+
+    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+    {
+
+		this->model = _model;
+		
+		this->velocity = math::Vector3();
+		this->velocity.Set(_sdf->Get<double>("X_velocity"), _sdf->Get<double>("Y_velocity"), _sdf->Get<double>("Z_velocity"));
+		this->SetVelocity(this->velocity);
+
+		this->orientation = math::Vector3();
+		this->orientation.Set(0,0,0);
+
+		this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+
+		this->rosSub = this->rosNode->subscribe("/gazebo/car_cmd", 10 , &CarControlPlugin::OnRosMsg, this);
+
+		this->rosQueueThread = std::thread(std::bind(&CarControlPlugin::QueueThread, this));
+    }
+
+    public: void SetVelocity(const gazebo::math::Vector3 &_vel)
+    {
+		this->model->SetLinearVel(_vel);
+		gzmsg << "Linear velocity set to: " << _vel.GetLength() << "\n";
+    }
+
+    public: void SetOrientation(const float &_theta)
+    {
+
+    	this->orientation.Set(0,0,_theta);
+    	math::Pose initPose(this->model->GetWorldPose().pos, math::Quaternion(0, 0, _theta));
+		this->model->SetWorldPose(initPose);
+		gzmsg << "Orientation set to: " << _theta << "\n";
+    }
+
+    public: void OnRosMsg(const display::ControlConstPtr &_msg)
+	{
+		this->SetOrientation(_msg->theta *PI/180.0);
+		this->velocity.Set(_msg->velocity*cos(_msg->theta *PI/180.0), _msg->velocity*sin(_msg->theta *PI/180.0), 0);
+    	this->SetVelocity(this->velocity);
+	}
+
+    /// \brief ROS helper function that processes messages
+	private: void QueueThread()
+	{
+	  static const double timeout = 0.01;
+	  while (this->rosNode->ok())
+	  {
+	    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+	  }
+	}
+
+	private: gazebo::math::Vector3 velocity;
+
+	private: gazebo::math::Vector3 orientation;
+
+    private: physics::ModelPtr model;
+
+	private: std::unique_ptr<ros::NodeHandle> rosNode;
+
+	private: ros::Subscriber rosSub;
+
+	private: ros::CallbackQueue rosQueue;
+
+	private: std::thread rosQueueThread;
+
+  };
+
+  GZ_REGISTER_MODEL_PLUGIN(CarControlPlugin)
+}
+
+#endif
---
+++ ProjetSupaero2018/catkin_ws/src/display/scripts/G_Display.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os
+import roslaunch
+import rospy, tf
+from rospy_tutorials.msg import Floats
+from std_msgs.msg import Float32
+from display.msg import Control
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import DeleteModel, SpawnModel
+from geometry_msgs.msg import *
+
+_car_name = 'my_car'
+
+def callbackJalon1(data):
+
+    #rospy.loginfo('I heard %f, %f', data.data[0], data.data[1])
+
+    pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=10)
+    
+    rate = rospy.Rate(10) # 10hz
+
+    msg = ModelState() 
+    msg.pose.position.x = data.data[0]
+    msg.pose.position.y = data.data[1]
+    msg.model_name = _car_name
+    pub.publish(msg)
+    rate.sleep()
+
+def callbackJalon2(data):
+
+    #rospy.loginfo('I heard %f, %f', data.data[0], data.data[1])
+
+    pub = rospy.Publisher('/gazebo/car_cmd', Control, queue_size=10)
+    
+    rate = rospy.Rate(10) # 10hz
+
+    vel = data.data[0]
+    theta = data.data[1]
+
+    msg = Control(vel, theta)
+    pub.publish(msg)
+    rate.sleep()
+
+def listener():
+
+    rospy.init_node('g_display', anonymous=True)
+
+    rospy.Subscriber('t_car_position', Floats, callbackJalon2)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+def initWorld():
+
+    #rospy.wait_for_service("gazebo/delete_sdf_model")
+    rospy.wait_for_service("gazebo/spawn_sdf_model")
+
+    #delete_sdf_model = rospy.ServicePoxy("gazebo/delete_sdf_model", DeleteModel)
+    spawn_sdf_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+
+    with open("src/display/models/my_car/model.sdf", "r") as f:
+        product_xml = f.read()
+
+    angle = tf.transformations.quaternion_from_euler(0,0,0)
+    orient = Quaternion(angle[0], angle[1], angle[2], angle[3])
+
+    item_pose   =   Pose(Point(x = 0, y = 0, z = 0),   orient)
+    spawn_sdf_model(_car_name, product_xml, "", item_pose, "world")
+
+if __name__ == '__main__':
+    
+    try:
+        initWorld()
+        listener()
+    except rospy.ROSInterruptException:
+         pass
