5d04f30b63898d4fabd072f355934e07b973a4a9
martinRenou <martin.renou@gmail.com>
2018-02-27 15:43:33 +0100

    Create simple client and launch files

---
+++ ProjetSupaero2018/catkin_ws/src/sampler/scripts/sampler_service_client_example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+from sampler.srv import Sample
+
+rospy.init_node('sampler_client')
+rospy.wait_for_service('sampler_service')
+
+try:
+    sampler_service = rospy.ServiceProxy('sampler_service', Sample)
+
+    print("Generating 20 samples:")
+    for _ in range(20):
+        resp = sampler_service()
+        print(resp.sample.x, resp.sample.y, resp.sample.z)
+
+except rospy.ServiceException, e:
+    print "Service call failed: %s" % e
