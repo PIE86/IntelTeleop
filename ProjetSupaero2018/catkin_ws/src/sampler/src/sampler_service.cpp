5b4c2ee87ff2e1ede5917a97e5702ccb88f50999
martinRenou <martin.renou@gmail.com>
2018-02-27 15:36:18 +0100

    Create first version of the sample creation service

---
+++ ProjetSupaero2018/catkin_ws/src/sampler/src/sampler_service.cpp
@@ -0,0 +1,39 @@
+#include <random>
+#include <math.h>
+
+#include "ros/ros.h"
+#include "geometry_msgs/Point.h"
+
+#include "sampler/Sample.h"
+
+bool create_sample(
+  beginner_tutorials::Sample::Request  &req,
+  beginner_tutorials::Sample::Response &res)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> generateFloat(0.0, 50.0);
+  std::uniform_real_distribution<> generateAngle(- M_PI, M_PI);
+
+  geometry_msgs::Point sample;
+  sample.x = generateFloat(gen);
+  sample.y = generateFloat(gen);
+  sample.z = generateAngle(gen);
+
+  res.sample = sample;
+
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "sampler_service");
+  ros::NodeHandle n;
+
+  ros::ServiceServer service = n.advertiseService(
+    "create_sample", create_sample);
+  ROS_INFO("Ready to create samples.");
+  ros::spin();
+
+  return 0;
+}
