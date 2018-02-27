14176c2736eb6cc540525e1cfc014040533e7f1a
martinRenou <martin.renou@gmail.com>
2018-02-27 18:48:45 +0100

    Change service answer type
    The sampler now creates nbSamples samples instead of one

--- ProjetSupaero2018/catkin_ws/src/opt_control/scripts/sampling_example.py
+++ ProjetSupaero2018/catkin_ws/src/opt_control/scripts/sampling_example.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import rospy
-from opt_control.srv import Sample
+from opt_control.srv import Samples
 
 rospy.init_node('sampling_client')
-rospy.wait_for_service('create_sample')
+rospy.wait_for_service('create_samples')
 
 try:
-    sampling_service = rospy.ServiceProxy('create_sample', Sample)
+    sampling_service = rospy.ServiceProxy('create_samples', Samples)
 
-    print("Generating 20 samples:")
-    for _ in range(20):
-        resp = sampling_service()
-        print(resp.sample.x, resp.sample.y, resp.sample.z)
+    print("Generating 30 samples:")
+    resp = sampling_service(30)
+    for sample in resp.samples:
+        print(sample.x, sample.y, sample.z)
 
 except rospy.ServiceException, e:
     print "Service call failed: %s" % e
--- ProjetSupaero2018/catkin_ws/src/opt_control/src/sampling_service.cpp
+++ ProjetSupaero2018/catkin_ws/src/opt_control/src/sampling_service.cpp
@@ -1,26 +1,32 @@
 #include <random>
 #include <math.h>
+#include <vector>
 
 #include "ros/ros.h"
 #include "geometry_msgs/Point.h"
 
-#include "opt_control/Sample.h"
+#include "opt_control/Samples.h"
 
 bool create_sample(
-  opt_control::Sample::Request  &req,
-  opt_control::Sample::Response &res)
+  opt_control::Samples::Request  &req,
+  opt_control::Samples::Response &res)
 {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> generateFloat(0.0, 50.0);
   std::uniform_real_distribution<> generateAngle(- M_PI, M_PI);
 
-  geometry_msgs::Point sample;
-  sample.x = generateFloat(gen);
-  sample.y = generateFloat(gen);
-  sample.z = generateAngle(gen);
+  std::vector<geometry_msgs::Point> samples;
+  for (int i = 0; i < req.nbSamples; i++){
+    geometry_msgs::Point sample;
+    sample.x = generateFloat(gen);
+    sample.y = generateFloat(gen);
+    sample.z = generateAngle(gen);
 
-  res.sample = sample;
+    samples.push_back(sample);
+  }
+
+  res.samples = samples;
 
   return true;
 }
@@ -31,7 +37,7 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   ros::ServiceServer service = n.advertiseService(
-    "create_sample", create_sample);
+    "create_samples", create_sample);
   ROS_INFO("Ready to create samples.");
   ros::spin();
 
