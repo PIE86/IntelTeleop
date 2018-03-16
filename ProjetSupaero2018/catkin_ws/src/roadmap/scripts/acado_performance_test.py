f5cb02c7a87d8ec04677457d1f64e14ba6257c4b
mfourmy <mederic.fourmy@isae.fr>
2018-03-16 13:37:26 +0100

    Refactoring acado performance test

--- ProjetSupaero2018/catkin_ws/src/roadmap/scripts/acado_performance_test.py
+++ ProjetSupaero2018/catkin_ws/src/roadmap/scripts/acado_performance_test.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+"""
+Script ploting 3 graphs exploring the relation between calculation time
+for the opt control, actual time of the trajectories and euclidian distance
+between start and end states. The opt_control can be initialized by the
+estimator so training should have been occured beforehand.
+To be launched with:
+roslaunch roadmap test_latency_acado_init.launch --screen
+"""
+
 import time
 import numpy as np
 from matplotlib import pyplot as plt
@@ -12,7 +21,9 @@ from irepa import Irepa, NX, NU
 
 OPT_CONTROL_SERVER = 'solve_ocp'
 
-NB_SAMPLES = 200
+NB_SAMPLES = 300
+# Initialize with the estimator
+INITIALIZE = True
 
 
 class LatencyTest:
@@ -31,19 +42,32 @@ class LatencyTest:
         self.irepa.estimator.load()
 
         self.calc_times = []
-        self.times = []
-        self.euclid_dist = []
+        self.traj_times = []
+        self.euclid = []
 
-    def test(self, end):
-        samples = self.irepa.sample(NB_SAMPLES)
+    def test_and_plot_results(self, end, nb_samples):
+        """Connect the samples and plot the results."""
+        self.test(end, nb_samples)
+        self.plot_results()
+
+    def test(self, end, nb_samples):
+        """
+        Generate nb_samples random states in the state space and tries to
+        them to the same end state. Then generate calcultation and trajectory
+        times and euclidian distance.
+        """
+
+        samples = self.irepa.sample(nb_samples)
 
         print('Start in 2 seconds')
         time.sleep(2)
         for state in samples:
-            Xe, Ue, Ve = self.irepa.estimator.trajectories(state, end)
-
-            Xe = Xe.flatten()
-            Ue = Ue.flatten()
+            if INITIALIZE:
+                Xe, Ue, Ve = self.irepa.estimator.trajectories(state, end)
+                Xe = Xe.flatten()
+                Ue = Ue.flatten()
+            else:
+                Xe, Ue, Ve = [], [], 0
 
             goal = OptControlGoal(
                 list(state),
@@ -54,43 +78,57 @@ class LatencyTest:
             self.ocp_client.wait_for_result(rospy.Duration.from_sec(5.0))
             t2 = time.time()
             self.calc_times.append(round(t2-t1, 2))
-            self.euclid_dist.append(round(self.irepa.euclid(state, end), 2))
+            self.euclid.append(round(self.irepa.euclid(state, end), 2))
+
+    def plot_results(self):
+        euclid_arr = np.array(self.euclid)
+        calc_times_arr = np.array(self.calc_times)
+        times_arr = np.array(self.traj_times)
+
+        # 1rst plot
+        plt.plot(euclid_arr, calc_times_arr,
+                 marker='.', linestyle='', label='Calculation times')
+        plt.xlabel('euclidian dist')
+        plt.ylabel('time (s)')
+        plt.title('Calculation time = f(euclidian distance)')
+        plt.xlim(0, 20)
+        plt.ylim(0, 2)
+        plt.legend()
+        plt.show()
+
+        # 2nd plot
+        plt.plot(euclid_arr, times_arr,
+                 marker='.', linestyle='', label='Trajectory times')
+        plt.xlabel('euclidian dist')
+        plt.plot(euclid_arr, times_arr - calc_times_arr,
+                 marker='.', linestyle='', label='Traj - Calc times')
+        plt.axhline(y=0, color='black', linestyle='-')
+        plt.xlabel('euclidian dist')
+        plt.ylabel('time (s)')
+        plt.title('Trajectory time = f(euclidian distance)')
+        plt.xlim(0, 20)
+        plt.ylim(-2, 20)
+        plt.legend()
+        plt.show()
+
+        # 3rd plot
+        plt.plot(times_arr,  calc_times_arr,
+                 marker='.', linestyle='')
+        plt.xlabel('Calculation time (s)')
+        plt.ylabel('Trajectory time (s)')
+        plt.title('Calculation time = f(Trajectory time)')
+        plt.legend()
+        plt.xlim(0, 20)
+        plt.ylim(0, 2)
+        plt.show()
 
     def callback(self, status, resp):
-        self.times.append(round(resp.time, 2))
+        self.traj_times.append(round(resp.time, 2))
 
 
 if __name__ == '__main__':
     end = np.array((12, 4, 0))
     ltest = LatencyTest()
-    ltest.test(end)
-    euclid_dist_arr = np.array(ltest.euclid_dist)
-    calc_times_arr = np.array(ltest.calc_times)
-    times_arr = np.array(ltest.times)
-
-    plt.plot(euclid_dist_arr, calc_times_arr,
-             marker='.', linestyle='', label='Calculation times')
-    plt.xlabel('euclidian dist')
-    plt.ylabel('time (s)')
-    plt.legend()
-    plt.show()
-    plt.plot(euclid_dist_arr, times_arr,
-             marker='.', linestyle='', label='Trajectory times')
-    plt.xlabel('euclidian dist')
-
-    plt.plot(euclid_dist_arr, times_arr - calc_times_arr,
-             marker='.', linestyle='', label='Traj - Calc times')
-    plt.axhline(y=0, color='black', linestyle='-')
-    plt.xlabel('euclidian dist')
-    plt.ylabel('time (s)')
-    plt.legend()
-    plt.show()
-
-    plt.plot(times_arr,  calc_times_arr,
-             marker='.', linestyle='')
-    plt.xlabel('Calculation time (s)')
-    plt.ylabel('Trajectory time (s)')
-    plt.legend()
-    plt.show()
+    ltest.test_and_plot_results(end, NB_SAMPLES)
 
     rospy.spin()
